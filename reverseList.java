56d0d91f221b607f0cb5b34f41ae6171195a0969
CSVignesh7 <csvignesh7@gmail.com>
2017-03-18 15:57:41 +0530

    Add files via upload

---
+++ reverseList.java
@@ -0,0 +1,11 @@
+    Public void ReverseList(LinkedList list){
+    	Node reversedPart = null;
+    	Node current = head;
+    	while (current != null) {
+    	    Node next = current.next;
+    	    current.next = reversedPart;
+    	    reversedPart = current;
+    	    current = next;
+    	}
+    	head = reversedPart;
+    }
