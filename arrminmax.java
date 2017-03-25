55401080c274b6060acc42eb72e48885cfca7040
CSVignesh7 <csvignesh7@gmail.com>
2017-03-25 22:54:59 +0530

    Update arrminmax.java

--- arrminmax.java
+++ arrminmax.java
@@ -3,7 +3,12 @@ public class arrminmax
 {
 	public static void main(String args[]){
 	   Scanner sc=new Scanner(System.in);
-        int arr[]={1,2,3,4,5,6};
+	int n=sc.nextInt();
+        int arr[]=new int[n];
+		for(int i=0;i<n;i++){
+	    arr[i]=sc.nextInt();
+		}
+		Arrays.sort(arr);
         int temp[]=new int[arr.length];
         int high=arr.length-1;
         int low=0;
